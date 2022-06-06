5adab6a08dd52b7cee9f71adde55e9dfbb882480
Cristian Cobo Ramirez <cristian.cobor@perficient.com>
2022-06-06 13:01:57 -0500

    location of global variables updated

--- cypress/integration/dress/dresses-list.spec.ts
+++ cypress/integration/dress/dresses-list.spec.ts
@@ -4,25 +4,27 @@ import {
   Dresses,
 } from "../../page";
 
+const menuContentPage: MenuContentPage = new MenuContentPage();
+const dresses: Dresses = new Dresses();
 
 describe("the user navigates to the dresses page should", () => {
-  let menuContentPage: MenuContentPage;
-  let dresses: Dresses;
+  let product: { dressesName: string[], dressesAmount:number };
 
-  before(() => {
-    menuContentPage = new MenuContentPage();
-    dresses = new Dresses();
+  beforeEach(()=>{
+    cy.fixture("products").then((tShirtName) =>{
+      product = tShirtName;
+    });
   });
 
   it("show the available amount of dresses", () => {
     menuContentPage.visitMenuContentPage();
     menuContentPage.goToDressesMenu();
-    dresses.validateItemsNumber(Cypress.env("dressesAmount"));
+    dresses.validateItemsNumber(product.dressesAmount);
   });
 
   it("the dress names appear correctly", () => {
     menuContentPage.visitMenuContentPage();
     menuContentPage.goToDressesMenu();
-    dresses.validateItemsNames(Cypress.env("dressesName"));
+    dresses.validateItemsNames(product.dressesName);
   });
 });
